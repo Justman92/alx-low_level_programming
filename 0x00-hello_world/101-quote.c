5d91ff6453b191437f03158abf4900f9fe860acc
Justice Anthony <justiceanthony690@gmail.com>
2022-06-30 18:38:54 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	printf("and that piece of art is useful
+	\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
