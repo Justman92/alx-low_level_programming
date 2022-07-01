faa648872f73a9d8461a2176ca11cc7d584e7dfc
Justice Anthony <justiceanthony690@gmail.com>
2022-07-01 16:36:53 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase
+ * Return: 0 if success
+ **/
+
+int main(void)
+{
+	char d = 'a';
+
+	while (d <= 'z')
+	{
+	putchar(d);
+	d++;
+	}
+	putchar('\n');
+	return (0);
+
+}
