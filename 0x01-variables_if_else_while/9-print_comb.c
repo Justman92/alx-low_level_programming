80441f453100eccda2d826e7bea1004b60830671
Justice Anthony <justiceanthony690@gmail.com>
2022-07-01 18:48:56 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Task 1 print the last digit of the number stored in the variable n
+ *
+ * Return: 0
+ **/
+int main(void)
+{
+	int ch;
+
+	for (ch = 48; ch <= 57; ch++)
+	{
+	putchar(ch);
+	if (ch != 57)
+	{
+	putchar(44);
+	putchar(32);
+	}
+	}
+	putchar(10);
+
+	return (0);
+
+}
