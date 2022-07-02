aea0648b327875234c1bb0ecd1ff0718b697e134
Justice Anthony <justiceanthony690@gmail.com>
2022-07-01 17:45:45 -0700

    Task 5

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,14 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  * main - prints the alphabet in lowercase, followed by a new line
  * Return: Always 0
  **/
 int main(void)
 {
-
 	char ch;
 	
 	for (ch = 'a'; ch <= 'z'; ch++)
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ * followed by a new line
+ *
+ * Return: 0 if success
+ **/
+
+int main(void)
+{
+	char d = '0';
+
+	while (d <= '9')
+	{
+	putchar(d);
+	d++;
+	}
+	putchar('\n');
+	return (0);
+
+}
