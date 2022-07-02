58b8f27e536db0e79f1ea7e6ff241566b1ae3a01
Justice Anthony <justiceanthony690@gmail.com>
2022-07-01 20:54:25 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,13 +5,23 @@
  **/
 int main(void)
 {
-	int num;
-	char letter;
+	int num1, num2;
 
-	for (num = 0; num < 10; num++)
-	putchar((num % 10) + '0');
-	for (letter = 'a'; letter <= 'f'; letter++)
-	putchar(letter);
+	for (num1 = 0; num1 <= 98; num1++)
+	{
+	for (num2 = num1 + 1; num2 <= 99; num2++)
+	{
+	putchar((num1 / 10) + '0');
+	putchar((num1 % 10) + '0');
+	putchar(' ');
+	putchar((num2 / 10) + '0');
+	putchar((num2 % 10) + '0');
+	if (num1 == 98 && num2 == 99)
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
 	putchar('\n');
 	return (0);
 }
