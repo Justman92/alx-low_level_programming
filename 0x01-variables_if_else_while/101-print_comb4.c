61144d21341e66b5286419cedd147a6f6c242850
Justice Anthony <justiceanthony690@gmail.com>
2022-07-01 20:46:11 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,37 +1,28 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 /**
- * main - Task 1 print the last digit of the number stored in the variable n
- *
- * Return: 0
+ * main - Entry point
+ * Return: Always 0
  **/
 int main(void)
 {
-	int ch;
-	int n;
-	int m;
+	int digit1, digit2, digit3;
 
-	for (ch = 48; ch <= 57; ch++)
+	for (digit1 = 0; digit1 < 8; digit1++)
 	{
-	for (n = 49; n <= 57; n++)
+	for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 	{
-	for (m = 50; m <= 57; m++)
+	for (digit3 = digit2 + 1; digit3 < 10; digit3++)
 	{
-	if (n > ch && m > n)
-	{
-	putchar(ch);
-	putchar(n);
-	putchar(m);
-	if (ch != 55 || n != 56 || m != 57)
-	{
-	putchar(44)	
-	putchar(32);
+	putchar((digit1 % 10) + '0');
+	putchar((digit2 % 10) + '0');
+	putchar((digit3 % 10) + '0');
+	if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+	continue;
+	putchar(',');
+	putchar(' ');
 	}
 	}
 	}
-	}
-	}
-	putchar(10);
+	putchar('\n');
 	return (0);
 }
