c76ce23595f269d3df6d3a1b991356406d9eac44
Justice Anthony <justiceanthony690@gmail.com>
2022-07-05 20:59:19 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit -> prints the last digit
+ * @n: passed argument
+ * Return: the last digit
+ */
+int print_last_digit(int n)
+{
+	int x;
+
+	if (n < 0)
+	n = -n;
+	x = n % 10;
+	_putchar (x + '0');
+	return (x);
+}
