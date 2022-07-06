04166b81b2c842b4ed93af231e36085941f00302
Justice Anthony <justiceanthony690@gmail.com>
2022-07-05 17:47:54 -0700

    print lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet -> prinys the lowercase alpahabets
+ */
+
+void print_alphabet(void)
+{
+	int j;
+
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
