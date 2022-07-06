a0668a34a2a518924f165e54f6af797e44998a0d
Justice Anthony <justiceanthony690@gmail.com>
2022-07-06 03:04:18 -0700

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,18 +1,32 @@
 #include "main.h"
-
 /**
- * Jack_bauer -> prinys 24 hours
- * Return: 
+ * jack_bauer - func that prints every minutes of the day of jack bauer, starti
+ * n from 00:00 to 23:59, min loop counts mins, while hour loop counts hours
+ * and resets mins
+ * Return: 0
  */
-void jack_bauer(void){
-	int i, j;
-	for (i = 0; i <24; i++)
+void jack_bauer(void)
+{
+	int hours = 0;
+	int minutes = 0;
+	int hours_remainder;
+	int mins_remainder;
+
+	while (hours <= 23)
 	{
-		for (j = 0; j , 60; j++)
-		{
-			_putchar(i);
-			_putchar(':');
-				_putchar(j)
-		}
+	while (minutes <= 59)
+	{
+	mins_remainder = minutes % 10;
+	hours_remainder = hours % 10;
+	_putchar(hours / 10 + '0');
+	_putchar(hours_remainder + '0');
+	_putchar(':');
+	_putchar(minutes / 10 + '0');
+	_putchar(mins_remainder + '0');
+	minutes++;
+	_putchar('\n');
+	}
+	hours++;
+	minutes = 0;
 	}
 }
