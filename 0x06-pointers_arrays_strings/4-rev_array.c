95c715a3937915f4e2407ae0b2e04b4648b40d40
Justice Anthony <justiceanthony690@gmail.com>
2022-07-15 04:38:21 -0700

    hot fix

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * reverse_array -> revserve the content of an array
+ * @a: array a
+ * @n: element of array
+ *
+ */
+void reverse_array(int *a, int n)
+{
+	int *p, i, aux, k;
+
+	p = a;
+	for (i = 0; i < n; i++)
+		p++;
+	for (k = 0; k < i / 2; k++)
+	{
+		aux = a[k];
+		a[k] = *p;
+		*p = aux;
+		p--;
+	}
+}
