8f4360c11c7801b380430460af81d98b397c01f4
Justice Anthony <justiceanthony690@gmail.com>
2022-07-15 04:17:24 -0700

    hoyfix

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp -> fo comparing purpose
+ * @s1: param1
+ * @s2: param2
+ * Return: string
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	result = strcmp(s1, s2);
+	return (result);
+}
+
