f9a9b5c6e0f7502463788884d809ba4ed41cf444
Justice Anthony <justiceanthony690@gmail.com>
2022-07-15 04:05:22 -0700

    hotfix

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy -> for copying purpose
+ * @dest: param1
+ * @src: param2
+ * @n: param3
+ *
+ * Return: string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/2-trncpy.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+/**
+ *
+ *
+ */
+char *_strncpy(char *dest, char *src, int n)
