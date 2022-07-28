416c2f38805b261b2bd9d5574d4344d3ad2fdf23
Justice Anthony <justiceanthony690@gmail.com>
2022-07-28 05:53:02 -0700

    task 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,11 +15,16 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
+
 	size = max - min + 1;
+
 	array = malloc(sizeof(int) * size);
+
 	if (array == NULL)
 		return (NULL);
+
 	for (index = 0; index < size; index++)
 		array[index] = min++;
+
 	return (array);
 }
