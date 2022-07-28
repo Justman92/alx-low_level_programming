189db5145cbe594acc132e61394a4fc43016ff21
Justice Anthony <justiceanthony690@gmail.com>
2022-07-28 05:59:18 -0700

    advanced 1

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -19,28 +19,32 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == old_size)
 		return (ptr);
+
 	if (ptr == NULL)
 	{
 		mem = malloc(new_size);
+
 		if (mem == NULL)
 			return (NULL);
-			return (mem);
+
+		return (mem);
 	}
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
-			return (NULL);
+		return (NULL);
 	}
 	ptr_copy = ptr;
 	mem = malloc(sizeof(*ptr_copy) * new_size);
 	if (mem == NULL)
 	{
 		free(ptr);
-			return (NULL);
+		return (NULL);
 	}
 	filler = mem;
+
 	for (index = 0; index < old_size && index < new_size; index++)
 		filler[index] = *ptr_copy++;
-			free(ptr);
+	free(ptr);
 	return (mem);
 }
